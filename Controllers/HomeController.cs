8c19915ee31dc69e19d55a717d9f8259f38bff5c
Emma Engstrom <engstrome48@gmail.com>
2023-03-01 10:32:12 -0700

    working version

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using onlineBookstoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using onlineBookstoreApp.Models.ViewModels;
+
+namespace onlineBookstoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IOnlineBookstoreRepository repo;
+
+        public HomeController (IOnlineBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+            var x = new IndexViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumEntries = repo.Books.Count(),
+                    EntriesPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+    }
+}
---
+++ Models/ViewModels/PageInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onlineBookstoreApp.Models
+{
+    public class PageInfo
+    {
+        public int TotalNumEntries { get; set; }
+        public int EntriesPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumEntries / EntriesPerPage);
+    }
+}
